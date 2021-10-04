a461f618f5aecb440bea974c35439aa8f87008e0
Guide Fari <guideg6@gmail.com>
2021-10-04 16:34:16 +0200

    holy shit headless mode records videos

---
+++ cypress/integration/examples/simple.spec.js
@@ -0,0 +1,18 @@
+describe('first simple test', () => {
+    it('true should be true', () => {
+        expect(true).to.equal(true)
+        expect(true).to.equal(true)
+    });
+    
+    it('5 should be 5', () => {
+        
+        expect(5).to.equal(5)
+    });
+})
+
+describe('another describe block', () => {
+    it('false should be false', () => {
+        
+        expect(false).to.equal(false)
+    });
+})
