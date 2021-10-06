8bafd5c872849d0dc77dd28cb0fdd2abf82ee626
Guide Fari <guideg6@gmail.com>
2021-10-06 09:14:38 +0200

    implicit wait and pause

--- cypress/integration/examples/simple.spec.js
+++ cypress/integration/examples/simple.spec.js
@@ -7,6 +7,16 @@ describe('Browser actions', () => {
         cy.url().should('include', 'example.com')
     });
 
+    // implicit wait
+    it('should wait for 3 seconds', () => {
+        cy.wait(3000)
+    });
+
+    // pause - good for debugging
+    it('should pause the thingy ma bob', () => {
+        cy.pause()
+    });
+    
     it('should find the correct elements on the page', () => {
         cy.get('h1').should('be.visible') 
     });
