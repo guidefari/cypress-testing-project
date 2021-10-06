2babb74119510d99024178b5ca1e43781b0d7b4e
Guide Fari <guideg6@gmail.com>
2021-10-06 08:57:01 +0200

    assertions

--- cypress/integration/examples/simple.spec.js
+++ cypress/integration/examples/simple.spec.js
@@ -1,18 +1,13 @@
-describe('first simple test', () => {
-    it('true should be true', () => {
-        expect(true).to.equal(true)
-        expect(true).to.equal(true)
+describe('Browser actions', () => {
+    it('should load correct url', () => {
+        cy.visit('http://example.com', {timeout: 10000})
     });
-    
-    it('5 should be 5', () => {
-        
-        expect(5).to.equal(5)
-    });
-})
 
-describe('another describe block', () => {
-    it('false should be false', () => {
-        
-        expect(false).to.equal(false)
+    it('should check correct url', () => {
+        cy.url().should('include', 'example.com')
+    });
+
+    it('should find the correct elements on the page', () => {
+        cy.get('h1').should('be.visible') 
     });
 })
